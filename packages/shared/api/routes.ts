e298dc95834f297c3592214666e17f263171c866
Glen Alangilan <glen@radzoft.com>
2023-02-19 19:26:15 +0800

    added shared package

---
+++ packages/shared/api/routes.ts
@@ -0,0 +1,4 @@
+
+export const AUTH = "/auth"
+export const AUTH_GENERATE="auth/generate"
+export const AUTH_VERIFY="auth/verify"
---
+++ packages/shared/index.ts
@@ -0,0 +1,6 @@
+export * as routes from './api/routes'
+export const test = ()=>{
+  return "test"
+}
+
+export {}
